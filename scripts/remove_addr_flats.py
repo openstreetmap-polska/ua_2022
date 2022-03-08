f256acddbc1d958be3eea21e6173fd6697e62b57
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-03-08 21:51:32 +0100

    Add remove_addr_flats script

---
+++ scripts/remove_addr_flats.py
@@ -0,0 +1,28 @@
+import json
+
+from os import path, walk
+from typing import Dict
+
+
+DIR = path.join('Roboczy')
+
+
+def rename_tag_addr_place_city(geojson: Dict) -> Dict:
+    for feature in geojson['features']:
+        if 'addr:flats' in feature['properties']:
+            del feature['properties']['addr:flats']
+
+    return geojson
+
+
+for root, subFolder, files in walk(DIR):
+    for geojson_filename in files:
+        filename = path.join(root, geojson_filename)
+
+        with open(filename, 'r', encoding='utf8') as f:
+            geojson = json.load(f)
+
+        modified_geojson = rename_tag_addr_place_city(geojson)
+
+        with open(filename, 'w', encoding='utf8') as f:
+            json.dump(modified_geojson, f, ensure_ascii=False, indent=4)
